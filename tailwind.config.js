b654edcf02a248d242415664415166138ec08c3a
guigocks-ux <guigocks@gmail.com>
2026-09-18 18:21:26 -0300

    fix: o modo claro nao deixado o menu claro

--- tailwind.config.js
+++ tailwind.config.js
@@ -48,6 +48,8 @@ module.exports = {
         'sidebar-muted': token('sidebar-muted'),
         'sidebar-border': token('sidebar-border'),
         'sidebar-active': token('sidebar-active'),
+        'sidebar-hover': token('sidebar-hover'),
+        'sidebar-strong': token('sidebar-strong'),
         'sidebar-accent': token('sidebar-accent'),
 
         'login-bg': token('login-bg'),
